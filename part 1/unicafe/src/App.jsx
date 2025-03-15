ba13e390d6f57d89146751fe76f55da2aa8b7d61
Darby Martin <darbymartinsignup@gmail.com>
2025-03-15 12:33:27 +1100

    Complete Step 1.6

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,23 +1,40 @@
 import { useState } from 'react'
 
+const Reviews = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  
   return (
     <div>
       <div><h1>give feedback</h1></div>
       <div>
-        buttons here...
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
       <div><h1>statistics</h1></div>
-      <div>
-        statistics here...
-      </div>
+        <Reviews good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
 
 export default App
+
+/* What I need
+- 3 buttons (good, neutral, bad)
+- 3 values that record how many good/neutral/bad button clicks are made
+
+*/
